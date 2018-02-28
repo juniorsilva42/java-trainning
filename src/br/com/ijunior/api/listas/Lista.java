362a8f72eb1ad751671be82a8df9f342353ceca1
jsiilva <jsiilva@outlook.com.br>
2018-02-28 00:38:13 -0300

    Adicionando o método de adicionar um elemento à lista

--- src/br/com/ijunior/api/listas/Lista.java
+++ src/br/com/ijunior/api/listas/Lista.java
@@ -16,4 +16,25 @@ public class Lista {
         this.lista = new String[capacidade];
         this.tamanho = 0;
     }
+
+    /*
+     *
+     * Adiciona um elemento à lista
+     *
+     */
+    public boolean add(String elemento) {
+
+        /*
+         *
+         * Se o tamanho da lista em contexto da classe é menor que a contagem de elementos da lista,
+         * então há espaço na lista, pode adicionar.
+         *
+         */
+        if (this.tamanho < this.lista.length) {
+            this.lista[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        }
+        return false;
+    }
 }
